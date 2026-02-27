dde9e3b9ad342cbe41b60c6d091fd7122d930c54
daniilgriga <daniilextra13@gmail.com>
2026-02-27 16:36:24 +0300

    [opencl-runtime]: added bitonic_runner interface (SortResult + bitonic_sort stub)

--- src/bitonic_runner.cpp
+++ src/bitonic_runner.cpp
@@ -1,1 +1,11 @@
 #include "bitonic_runner.hpp"
+
+namespace bitonic
+{
+
+    SortResult bitonic_sort (ocl::Runtime& /*rt*/, std::vector<int>& /*data*/)
+    {
+        return {0, 0};
+    }
+
+} // namespace bitonic
